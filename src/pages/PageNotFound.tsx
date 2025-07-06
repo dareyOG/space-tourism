d90085dfac0cabb9e16d11f1d7fa94843aad13b4
Oludire Damilare <oludire@gmail.com>
2025-07-06 20:42:06 +0100

    ✅page-not-found

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,5 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+
 function PageNotFound() {
-  return <div>Page Not Found</div>;
+  const navigate = useNavigate();
+
+  const handleMoveBack = () => navigate(-1);
+  return (
+    <div className="h-screen bg-dark-blue flex flex-col gap-y-5 text-center text-light-blue justify-center items-center">
+      <h1 className="text-2xl capitalize ">Page not found 🔎❗</h1>
+      <button
+        onClick={handleMoveBack}
+        className="border px-[2rem] py-[1rem] rounded-full flex gap-x-2 hover:bg-light-blue hover:text-dark-blue"
+      >
+        <span>&larr;</span>
+        <>Go back</>
+      </button>
+    </div>
+  );
 }
 
 export default PageNotFound;
